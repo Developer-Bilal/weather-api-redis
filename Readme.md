2f1fdc50fdac2001f13597c0b3513fdb9af2a378
Muhammad Bilal <bilalchanna67@gmail.com>
2025-04-29 07:10:52 +0500

    learned caching Redis

--- Readme.md
+++ Readme.md
@@ -1,5 +1,10 @@
 # Caching Weather API
 
-### Step-1
+Learned about caching and how to cache data in Redis.
 
-- Create basic express server
+### Steps
+
+1. Create basic express server
+2. Get weather data from weather API
+3. Create Redis Client and connect
+4. Function logic => if data is in cache return (GET) it else cache (SET) the data and return it.
--- server.js
+++ server.js
@@ -1,18 +1,55 @@
 import express from "express";
+import { createClient } from "redis";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+// redis connection setup
+const client = createClient({
+  username: "default",
+  password: process.env.Redis_Password,
+  socket: {
+    host: process.env.Redis_Host,
+    port: process.env.Redis_Port,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+await client.connect();
 
 // middlewares
 app.use(express.json());
 
 // routes
-app.get("/api/v1", getWeatherData);
+app.get("/api/v1/:location", getWeatherData);
 
 // route controllers
 async function getWeatherData(req, res) {
-  console.log("getWeatherData");
-  res.status(200).json({ message: "getWeatherData" });
+  let myData = "";
+  // get data: cache data from redis
+  const CachedData = await client.get(myData);
+  if (CachedData) {
+    res
+      .status(200)
+      .json({ status: "Success!", cache: true, data: JSON.parse(CachedData) });
+  } else {
+    try {
+      const { location } = req.params;
+
+      const resp = await fetch(
+        `${process.env.BASE_URL}/${location}?unitGroup=metric&key=${process.env.API_KEY}&contentType=json`
+      );
+      const data = await resp.json();
+      // save data: cache data in redis
+      await client.set(myData, JSON.stringify(data), { EX: 60 });
+
+      res.status(200).json({ status: "Success!", cache: false, data });
+    } catch (error) {
+      res.status(500).json({ message: "Server Error", error: error.message });
+    }
+  }
 }
 
 app.listen(port, () => {
